ecf7c2c1cbc261d097212e2615615ab5d91afe98
Djones A. Boni <djboni@gmail.com>
2021-03-31 00:01:07 -0300

    TRACE32 breakpoint conversion

--- README.md
+++ README.md
@@ -6,18 +6,45 @@ This can help you to convert breakpoints from VS Code to a format accepted in an
 
 The advantage of setting breakpoints in VS Code is that they are dynamic, so they move up and down with the code as lines are removed or inserted.
 
-# Example
+# Real example
+
+Convert VS Code breakpoints to TRACE32 breakpoints.
+
+Copy and paste the breakpoints in TRACE32 to set them. You can also insert them in your CMM file.
+
+```python
+# Convert VS Code breakpoints to TRACE32 breakpoints
+from breakpoint_magick import GetVSCodeBreakpoints
+from breakpoint_magick import SetTrace32Breakpoints
+breakpoint_list = GetVSCodeBreakpoints()
+trace32_breakpoint_str = SetTrace32Breakpoints(breakpoint_list)
+print(trace32_breakpoint_str)
+```
+
+Example output:
+
+```
+Break.RESet
+Break.Set AES_ECBEncrypt /Program
+Break.Set AES_ECBEncrypt\18 /Program
+Break.Set AES_ECBDecrypt /Program
+Break.Set AES_ECBDecrypt\16 /Program
+```
+
+# Dummy example
+
+Convert VS Code breakpoints to a made-up language.
 
 ```python
 # Import things
 from breakpoint_magick import GetVSCodeBreakpoints
 
 # Get a list with VS Code breakpoints
-break_point_list = GetVSCodeBreakpoints()
+breakpoint_list = GetVSCodeBreakpoints()
 
 # Example of processing to new format
 print("# Breakpoints")
-for brk in break_point_list:
+for brk in breakpoint_list:
     file_name = brk.file.replace('"', '\\"')
     file_line = brk.line
     enabled = 'Enabled' if brk.enabled else 'Disabled'
@@ -34,4 +61,13 @@ for brk in break_point_list:
         print("SetBreakpointCondition \"%s\" %d %s \"%s\"" % (file_name, file_line, enabled, cond))
     else:
         print("SetBreakpoint \"%s\" %d %s" % (file_name, file_line, enabled))
+```
+
+Example output:
+
+```
+SetBreakpoint "DIR/source/aes.c" 467 Enabled
+SetBreakpoint "DIR/source/aes.c" 485 Enabled
+SetBreakpoint "DIR/source/aes.c" 531 Enabled
+SetBreakpoint "DIR/source/aes.c" 547 Enabled
 ```
--- breakpoint_magick.py
+++ breakpoint_magick.py
@@ -11,17 +11,23 @@ import glob
 import sqlite3
 import json
 import collections
+import re
 
 BreakpointTuple = collections.namedtuple('Breakpoint', ['file', 'line', 'enabled', 'hit_count', 'cond'])
 
 def GetVSCodeBreakpoints():
+    # VS Code uses file names and lines of the files to set breakpoints.
+    #
+    # This function outputs a list of tuples that contain breakpoint information
+    # extracted form VS Code.
+    #
     # VS Code saves breakpoints in a SQLite3 database, in the table ItemTable,
     # with key='debug.breakpoint', and value=JSON_DATA.
     # The databases can be found in:
     #     Windows %APPDATA%\Code\User\workspaceStorage\*\state.vscdb
     #     Linux   $HOME/.config/Code/User/workspaceStorage/*/state.vscdb
 
-    break_point_list = list()
+    breakpoint_list = list()
 
     # OS dependent path for databases
     if os.name == 'nt':
@@ -32,7 +38,7 @@ def GetVSCodeBreakpoints():
         raise Exception('Unknown os.name: "%s"' % os.name)
 
     # Open all the state.vscdb SQLite3 databases and output breakpoint data to
-    # the list 'break_point_list'
+    # the list 'breakpoint_list'
     for file_path in glob.glob(path_breakpoints):
         with sqlite3.connect(file_path) as con:
             # Read SQLite3 database
@@ -45,7 +51,7 @@ def GetVSCodeBreakpoints():
                     brk_list = json.loads(json_data)
                     # Process breakpoints
                     for brk in brk_list:
-                        # Add each breakpoint to the list 'break_point_list'
+                        # Add each breakpoint to the list 'breakpoint_list'
                         brk_path = brk['uri']['path']
                         if os.name == 'nt':
                             # Fix path for Windows: remove prefixed backslash
@@ -65,30 +71,91 @@ def GetVSCodeBreakpoints():
                             brk_cond = None
 
                         brk_tuple = BreakpointTuple(brk_path, brk_line, brk_en, brk_hit, brk_cond)
-                        break_point_list.append(brk_tuple)
+                        breakpoint_list.append(brk_tuple)
 
-    return break_point_list
+    return breakpoint_list
+
+def SetTrace32Breakpoints(breakpoint_list):
+    # TRACE32 uses function names and lines of the functions to set breakpoints.
+    #
+    # This function outputs a string that contain breakpoint information in
+    # TRACE32 language (named PRACTICE).
+    #
+    # This function converts (file,file_line) breakpoints into
+    # (function,func_line) and writes breakpoints to TRACE32 language.
+
+    trace32_breakpoint_str = 'Break.RESet\n'
+
+    # Regex to process files
+    # Accept only .C source files
+    accepted_files_regex = r'''.*?\.[cC]$'''
+    accepted_files_regex = re.compile(accepted_files_regex)
+
+    # Regex to find function definition
+    #                     Function Return Ptr*   FunctionName   (args   )    {
+    func_def_regex = r'''^([a-zA-Z0-9_]+[\s\*]+)+([a-zA-Z0-9_]+)\([^\)]*\)\s*\{'''
+    func_def_regex = re.compile(func_def_regex, re.DOTALL)
+
+    # For each breakpoint
+    for brk in breakpoint_list:
+
+        if accepted_files_regex.match(brk.file) == None:
+            # Do not process this type of file
+            print('Ignoring file: %s' % brk.file)
+            continue
+
+        # Read file
+        with open(brk.file, 'r') as fp:
+            source_lines = fp.readlines()
+
+        # Find function definition using regex
+        function_source = ''
+        for i in range(brk.line - 1, -1, -1):
+            function_source = source_lines[i] + function_source
+            res = func_def_regex.match(function_source)
+            if res != None:
+                break
+        else:
+            s = "Function not found for %s" % str(brk)
+            raise Exception(s)
+
+        # Get function name and line number
+        function_name = res.group(2)
+        function_line = brk.line -1 - i - res.group(0).count('\n')
+        function_line = '' if function_line == 0 else ("\\%d" % function_line)
+        enabled = '' if brk.enabled else " /DISable"
+        hit_count = '' if brk.hit_count == None else (" /COUNT %d." % brk.hit_count)
+        cond = '' if brk.cond == None else (" /VarCONDition (%s)" % brk.cond)
+
+        trace32_breakpoint_str += """Break.Set %s%s /Program%s%s%s\n""" % (function_name, function_line, enabled, hit_count, cond)
+
+    return trace32_breakpoint_str
 
 if __name__ == '__main__':
     # Get a list with breakpoints
-    break_point_list = GetVSCodeBreakpoints()
+    breakpoint_list = GetVSCodeBreakpoints()
 
-    # Dummy example of processing to new format
-    print("# Breakpoints")
-    for brk in break_point_list:
-        file_name = brk.file.replace('"', '\\"')
-        file_line = brk.line
-        enabled = 'Enabled' if brk.enabled else 'Disabled'
-        hit_count = brk.hit_count
-        cond = brk.cond
-        if cond != None:
-            cond = cond.replace('"', '\\"')
+    if True:
+        # Convert to TRACE32 breakpoints
+        trace32_breakpoint_str = SetTrace32Breakpoints(breakpoint_list)
+        print(trace32_breakpoint_str)
+    else:
+        # Dummy example of processing to new format
+        print("# Breakpoints")
+        for brk in breakpoint_list:
+            file_name = brk.file.replace('"', '\\"')
+            file_line = brk.line
+            enabled = 'Enabled' if brk.enabled else 'Disabled'
+            hit_count = brk.hit_count
+            cond = brk.cond
+            if cond != None:
+                cond = cond.replace('"', '\\"')
 
-        if hit_count != None and cond != None:
-            print("SetBreakpointHitCountCondition \"%s\" %d %s %d \"%s\"" % (file_name, file_line, enabled, hit_count, cond))
-        elif hit_count != None:
-            print("SetBreakpointHitCount \"%s\" %d %s %d" % (file_name, file_line, enabled, hit_count))
-        elif cond != None:
-            print("SetBreakpointCondition \"%s\" %d %s \"%s\"" % (file_name, file_line, enabled, cond))
-        else:
-            print("SetBreakpoint \"%s\" %d %s" % (file_name, file_line, enabled))
+            if hit_count != None and cond != None:
+                print("SetBreakpointHitCountCondition \"%s\" %d %s %d \"%s\"" % (file_name, file_line, enabled, hit_count, cond))
+            elif hit_count != None:
+                print("SetBreakpointHitCount \"%s\" %d %s %d" % (file_name, file_line, enabled, hit_count))
+            elif cond != None:
+                print("SetBreakpointCondition \"%s\" %d %s \"%s\"" % (file_name, file_line, enabled, cond))
+            else:
+                print("SetBreakpoint \"%s\" %d %s" % (file_name, file_line, enabled))
